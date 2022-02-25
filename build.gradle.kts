9b9bc000f463217cd6030679d1862a4015d3892a
IamMaxim <g.maxim.stepanoff@gmail.com>
2022-02-25 12:53:15 +0300

    Added Maven release configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,10 +3,19 @@ import java.security.MessageDigest
 
 plugins {
     java
+    `maven-publish`
+    signing
 }
 
 group = "org.polystat"
 version = "0.0.1"
+
+
+val mvnUsername: String? by project
+val mvnPassword: String? by project
+val mvnPublicationVersion: String? by project
+val candidates: String? by project
+
 
 // The Java grammar source file for Bison
 val javaGrammarFilePath = "src/main/resources/Java_16_Grammar.y"
@@ -50,6 +59,69 @@ tasks.getByName("build") {
     }
 }
 
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            groupId = "org.polystat"
+            artifactId = "j2ast"
+            version = project.version as String
+            from(components["java"])
+            pom {
+                name.set("j2ast")
+                description.set("Java parser")
+                url.set("https://github.com/polystat/j2ast")
+                // properties.set(mapOf(
+                //     "myProp" to "value",
+                //     "prop.with.dots" to "anotherValue"
+                // ))
+                licenses {
+                    license {
+                        name.set("MIT")
+                        url.set("https://github.com/polystat/polystat/blob/master/LICENSE.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("zouev")
+                        name.set("Eugene Zouev")
+                        email.set("e.zuev@innopolis.ru")
+                    }
+                    developer {
+                        id.set("IamMaxim58")
+                        name.set("Maxim Stepanov")
+                        email.set("m.stepanov@innopolis.university")
+                    }
+                    developer {
+                        id.set("Ilia_Mil")
+                        name.set("Ilya Milyoshin")
+                        email.set("i.mileshin@innopolis.university")
+                    }
+                    developer {
+                        id.set("egorklementev")
+                        name.set("Egor Klementev")
+                        email.set("e.klementev@innopolis.ru")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git@github.com:polystat/j2ast.git")
+                    developerConnection.set("scm:git:git@github.com:polystat/j2ast.git")
+                    url.set("https://github.com/polystat/j2ast")
+                }
+            }
+            repositories {
+                maven {
+                    credentials {
+                        println("Applying Maven credentials")
+
+                        username = mvnUsername
+                        password = mvnPassword
+                    }
+                    url = uri("https://s01.oss.sonatype.org/content/repositories/releases/")
+                }
+            }
+        }
+    }
+}
 
 /**
  * Creates directories for all Bison output files.
@@ -68,50 +140,52 @@ fun createOutDirs() {
  * Runs Bison using OS-specific shell command.
  */
 fun runBison() =
-        try {
-            when {
-                Os.isFamily(Os.FAMILY_WINDOWS) ->
-                    exec {
-                        workingDir = File(".")
-                        executable = "bin/win_bison.exe"
-                        args = mutableListOf(
-                                "--report=states,lookaheads",
-                                // "-r", "all",
-                                // "--debug", "--help", "--stacktrace",
-                                "--report-file=${reportFilePath}",
-                                "--output=${javaParserFilePath}",
-                                javaGrammarFilePath
-                        )
-                    }
-                Os.isFamily(Os.FAMILY_MAC) ->
-                    exec {
-                        workingDir = File(".")
-                        executable = "bin/bison_mac"
-                        args = mutableListOf(
-                                "--report=states,lookaheads",
-                                "--report-file=${reportFilePath}",
-                                "--output=${javaParserFilePath}",
-                                javaGrammarFilePath
-                        )
-                    }
-                Os.isFamily(Os.FAMILY_UNIX) ->
-                    exec {
-                        workingDir = File(".")
-                        executable = "bison"
-                        args = mutableListOf(
-                                "--report=states,lookaheads",
-                                "--report-file=${reportFilePath}",
-                                "--output=${javaParserFilePath}",
-                                javaGrammarFilePath
-                        )
-                    }
-                else ->
-                    throw UnsupportedOperationException("Your OS is not yet supported. File a GitHub or issue or " +
-                            "provide a Pull Request with support for Bison execution for your OS.")
-            }
-        } catch (e: Exception) {
-            e.printStackTrace()
+    try {
+        when {
+            Os.isFamily(Os.FAMILY_WINDOWS) ->
+                exec {
+                    workingDir = File(".")
+                    executable = "bin/win_bison.exe"
+                    args = mutableListOf(
+                        "--report=states,lookaheads",
+                        // "-r", "all",
+                        // "--debug", "--help", "--stacktrace",
+                        "--report-file=${reportFilePath}",
+                        "--output=${javaParserFilePath}",
+                        javaGrammarFilePath
+                    )
+                }
+            Os.isFamily(Os.FAMILY_MAC) ->
+                exec {
+                    workingDir = File(".")
+                    executable = "bin/bison_mac"
+                    args = mutableListOf(
+                        "--report=states,lookaheads",
+                        "--report-file=${reportFilePath}",
+                        "--output=${javaParserFilePath}",
+                        javaGrammarFilePath
+                    )
+                }
+            Os.isFamily(Os.FAMILY_UNIX) ->
+                exec {
+                    workingDir = File(".")
+                    executable = "bison"
+                    args = mutableListOf(
+                        "--report=states,lookaheads",
+                        "--report-file=${reportFilePath}",
+                        "--output=${javaParserFilePath}",
+                        javaGrammarFilePath
+                    )
+                }
+            else ->
+                throw UnsupportedOperationException(
+                    "Your OS is not yet supported. File a GitHub or issue or " +
+                            "provide a Pull Request with support for Bison execution for your OS."
+                )
         }
+    } catch (e: Exception) {
+        e.printStackTrace()
+    }
 
 
 /**
@@ -138,15 +212,15 @@ fun generateMD5(filepath: String): String {
 }
 
 fun grammarFileMD5(): String =
-        generateMD5(javaGrammarFilePath)
+    generateMD5(javaGrammarFilePath)
 
 fun readMD5FromFile(filepath: String): String =
-        File(filepath).let { f ->
-            if (f.exists())
-                File(filepath).readText()
-            else
-                ""
-        }
+    File(filepath).let { f ->
+        if (f.exists())
+            File(filepath).readText()
+        else
+            ""
+    }
 
 fun writeMD5ToFile(md5: String, filepath: String) =
-        File(filepath).writeText(md5)
+    File(filepath).writeText(md5)
